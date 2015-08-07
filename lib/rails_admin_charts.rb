899ce86edb410bc4223fc8c123771aba0a3bc26d
Danik <31.10.93@bk.ru>
2015-08-07 13:14:11 +0600

    Changed total_records_since to support mongoid

--- lib/rails_admin_charts.rb
+++ lib/rails_admin_charts.rb
@@ -9,10 +9,20 @@ module RailsAdminCharts
   module ClassMethods
 
     def total_records_since(since = 30.days.ago)
-      date_created_at = "Date(#{self.table_name}.created_at)"
-      totals, before_count = self.group(date_created_at).count, self.where(date_created_at + ' < ?', since.to_date).count
+      #date_created_at = "Date(#{self.table_name}.created_at)"
+      #totals, before_count = self.group(date_created_at).count, self.where(date_created_at + ' < ?', since.to_date).count
       # TODO separate MySQL/Postgres approaches using ActiveRecord::Base.connection.adapter_name or check hash key is_a? String/Date
-      (since.to_date..Date.today).each_with_object([]) { |day, a| a << (a.last || before_count) + (totals[day] || totals[day.to_s] || 0) }
+      
+	  days_between = (Date.today - since).to_i
+	  
+	  totals = Hash.new(0)
+	  days_between.times do |s|
+		totals[(Date.today - s.days).to_date] = self.(:created_at.gte => Date.today - s.days, :created_at.lt => Date.today - (s - 1).days).count
+	  end
+	  
+	  before_count = self.where(:created_at.lte => Date.today - days_between.days).count
+	  
+	  (since.to_date..Date.today).each_with_object([]) { |day, a| a << (a.last || before_count) + (totals[day] || totals[day.to_s] || 0) }
     end
 
     def delta_records_since(since = 30.days.ago)
